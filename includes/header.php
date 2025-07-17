3d4ef1df024b12a73aa5a8c42ff977afd4e40c02
Imran0316 <ik775239@gmail.com>
2025-07-17 12:00:48 +0500

    styling setup for login page

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,8 @@
 
     <title>Banquet Booking System</title>
 
+    
+
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
@@ -22,6 +24,10 @@
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
 
+    <link rel="stylesheet" href="../assets/css/login.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+ 
+    
 
     <link rel="icon" href="favicon.ico" type="image/x-icon" />
 </head>
